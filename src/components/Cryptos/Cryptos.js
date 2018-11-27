bd0c63f8791b91e26407a55a2e81ccecd35cd015
JGBrown <j.grant.brown@gmail.com>
2018-11-27 16:41:48 -0500

    stub out the page with a button for opening an add coins

--- src/components/Cryptos/Cryptos.js
+++ src/components/Cryptos/Cryptos.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+// import InputHolding from './InputHolding'
 
 class Cryptos extends Component {
+
+  handleAddCoin = e => {
+    e.preventDefault()
+    debugger
+    // Render the </InputHolding>
+  }
+
   render() {
+
+    // const addForm =
+
+
+
     return (
       <div>
-        THis Cryptos Component Soon to be a list of Cryptos with QUotes
+        <p>THis Cryptos Component Soon to be a list of Cryptos with QUotes</p>
+        <button onClick={(e)=>this.handleAddCoin(e)}>Add More coins</button>
       </div>
     )
   }}
---
+++ src/components/Cryptos/InputHolding.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class InputHolding extends Component {
+  render() {
+    return (
+      <div>
+        THis Input Form Component Soon to be a here
+        <form>
+          <input type="text" value="token"/><br/>
+          <input type="text" value="amount"/><br/>
+          <input type="text" value="cost basis"/><br/>
+          <input type="text" value="wallet"/><br/>
+          <input type="submit" />
+        </form>
+      </div>
+    )
+  }}
+
+  export default InputHolding;
