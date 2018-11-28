175f7ef3c6dfba04b5744b09591705daf5ace0da
JGBrown <j.grant.brown@gmail.com>
2018-11-28 12:36:35 -0500

    handle form entry data change

--- src/components/Cryptos/InputHolding.js
+++ src/components/Cryptos/InputHolding.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
 
 class InputHolding extends Component {
+
+  constructor(props) {
+      super(props);
+      this.state = {
+        token: "",
+        amount: "",
+        costbasis: "",
+        wallet: ""
+      };
+
+    }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    debugger
+    // USe a route to rerender and expose the form
+    // Render the </InputHolding>
+  }
+
+  handleChange = evt => {
+    debugger
+    this.setState({ [evt.target.name]: evt.target.value });
+    // Set up form changes
+  }
+
+
+
   render() {
     const showForm = (this.props.visible) ?
-      <form>
-        <input type="text" value="token"/><br/>
-        <input type="text" value="amount"/><br/>
-        <input type="text" value="cost basis"/><br/>
-        <input type="text" value="wallet"/><br/>
+      <form onSubmit={(e)=>this.Submit(e)}>
+        <input type="text" value="token" onChange={this.handleChange}/><br/>
+        <input type="text" value="amount" onChange={this.handleChange}/><br/>
+        <input type="text" value="cost basis" onChange={this.handleChange}/><br/>
+        <input type="text" value="wallet" onChange={this.handleChange}/><br/>
         <input type="submit" />
       </form> : <div> </div>
 
