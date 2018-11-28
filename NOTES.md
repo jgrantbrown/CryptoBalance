3fce985819c71e022c52d6225fa7b99ad2a066c1
JGBrown <j.grant.brown@gmail.com>
2018-11-28 16:52:24 -0500

    added reactstrap now tinkering with containers and rows

---
+++ NOTES.md
@@ -0,0 +1,27 @@
+{/* <div style={{display: "inline-block",
+backgroundColor: "gray",
+textAlign: "center",
+margin: "0",
+padding: "0",
+listStyleType: "none"
+}}> */}
+    // <div style={{ backgroundColor: "#44014C", width: "175px", minHeight: "100px"}}>
+    //   <h2 style={{ padding: "10px 10px", textAlign: "center", color: "white"}}>
+    //     ETH:
+    //     $117.00
+    //   </h2>
+    // </div>
+    //
+    // <div style={{ backgroundColor: "#44014C", width: "175px", minHeight: "100px"}}>
+    //     <h2 style={{ padding: "10px 10px", textAlign: "center", color: "white"}}>
+    //       BTC:
+    //       $117.00
+    //     </h2>
+    //   </div>
+    //   <div style={{ backgroundColor: "#44014C", width: "175px", minHeight: "100px"}}>
+    //       <h2 style={{ padding: "10px 10px", textAlign: "center", color: "white"}}>
+    //         LTC
+    //         $117.00
+    //       </h2>
+    //   </div>
+// </div>
--- src/components/Cryptos/CryptoPrices.js
+++ src/components/Cryptos/CryptoPrices.js
@@ -1,31 +1,34 @@
 import React, { Component } from 'react';
+import { Container, Row, Col } from 'reactstrap';
 
 class CryptoPrices extends Component {
   render(){
     return(
-      <div>
-          <div style={{ backgroundColor: "#44014C", width: "175px", minHeight: "100px"}}>
-            <h2 style={{ padding: "10px 10px", textAlign: "center", color: "white"}}>
-              ETH:
-              $117.00
-            </h2>
-            </div>
-            <div style={{ backgroundColor: "#44014C", width: "175px", minHeight: "100px"}}>
-              <h2 style={{ padding: "10px 10px", textAlign: "center", color: "white"}}>
-                BTC:
-                $117.00
-              </h2>
-              </div>
-              <div style={{ backgroundColor: "#44014C", width: "175px", minHeight: "100px"}}>
+      <Container>
+        <Row>
+          <Col style={{ backgroundColor: "#44014C", width: "175px", minHeight: "100px"}}>
                 <h2 style={{ padding: "10px 10px", textAlign: "center", color: "white"}}>
-                  LTC
+                  ETH:
                   $117.00
-                </h2>
-              </div>
-      </div>
+                 </h2></Col>
+        <Col style={{ backgroundColor: "#44014C", width: "175px", minHeight: "100px"}}>
+              <h2 style={{ padding: "10px 10px", textAlign: "center", color: "white"}}>
+                ETH:
+                $117.00
+               </h2></Col>
+        <Col style={{ backgroundColor: "#44014C", width: "175px", minHeight: "100px"}}>
+                 <h2 style={{ padding: "10px 10px", textAlign: "center", color: "white"}}>
+                   ETH:
+                   $117.00
+                  </h2></Col>
+       </Row>
+      </Container>
+
     )
   }
 
 }
+
+
 
 export default CryptoPrices;
