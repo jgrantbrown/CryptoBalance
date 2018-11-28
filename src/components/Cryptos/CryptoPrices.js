94c42b02d02168d0065c95fe8b729ac1ca4a6238
JGBrown <j.grant.brown@gmail.com>
2018-11-28 15:46:06 -0500

    setting up cards for coin quotes

--- src/components/Cryptos/CryptoPrices.js
+++ src/components/Cryptos/CryptoPrices.js
@@ -3,10 +3,25 @@ import React, { Component } from 'react';
 class CryptoPrices extends Component {
   render(){
     return(
-        <div>
-          <div style={{ backgroundColor: "#44014C", width: "300px", minHeight: "200px"}}>
-        <h2 style={{ padding: "10px 20px", textAlign: "center", color: "white"}}>CRYPTO PRICE BOX</h2>
-        </div>
+      <div>
+          <div style={{ backgroundColor: "#44014C", width: "175px", minHeight: "100px"}}>
+            <h2 style={{ padding: "10px 10px", textAlign: "center", color: "white"}}>
+              ETH:
+              $117.00
+            </h2>
+            </div>
+            <div style={{ backgroundColor: "#44014C", width: "175px", minHeight: "100px"}}>
+              <h2 style={{ padding: "10px 10px", textAlign: "center", color: "white"}}>
+                BTC:
+                $117.00
+              </h2>
+              </div>
+              <div style={{ backgroundColor: "#44014C", width: "175px", minHeight: "100px"}}>
+                <h2 style={{ padding: "10px 10px", textAlign: "center", color: "white"}}>
+                  LTC
+                  $117.00
+                </h2>
+              </div>
       </div>
     )
   }
