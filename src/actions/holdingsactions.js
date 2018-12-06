9a47ff55f77ba0ebbdd1f296a5b7f06323e50b62
JGBrown <j.grant.brown@gmail.com>
2018-12-06 16:05:07 -0500

    need to work ona action after clicking portfolio name to fetch holdings data and render

--- src/actions/holdingsactions.js
+++ src/actions/holdingsactions.js
@@ -10,32 +10,10 @@ export const postPortfolio = (url, data)=>{
     body: JSON.stringify(data)
   })
   .then(response => response.json())
-  // .catch(error => console.error('Error:', error));
+  .catch(error => console.error('Error:', error));
 }
 
-// SHOULD IT BE THUNK
-// function postPortfolio(url, data){
-// retrun dispatch =>{
-//   dispatch(addPortfolio());
-//   return fetch(url)
-//   .then(res => res.json())
-//   .then(parsedRes =>{
-//     dispatch({
-//       type: "ADD_PORTFOLIO"
-//       data: parsedRes.name
-//     })
-//
-//   })
-// }
-// }
-
-
 export const addHolding = (holding)=>{
-  console.log("Action Holding:",holding)
-
-  // Need to Post the new portfolio name
-// fetch(localhost:3001/portfolios)
-// .then((response))=>
   return {
     type: 'ADD_HOLDING',
     payload: holding
